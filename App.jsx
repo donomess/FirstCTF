cc9aeb850e56226b09c15a057cc4a86d8189b8e3
Donovan Messer <donovanmess01@gmail.com>
2022-02-18 15:52:16 -0500

    made additions to ctf. made flag and flagchecker

---
+++ App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return(
+        <div className ="App">
+            <header>
+                <h1>please get the flag thanks</h1>
+            </header>
+        </div>
+    );
+}
