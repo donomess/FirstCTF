1fdbccbdd04fe0a1493d79b4ea22d9fac1436582
Donovan Messer <donovanmess01@gmail.com>
2022-02-18 16:03:48 -0500

    this might work idk

--- App.jsx
+++ App.jsx
@@ -1,9 +1,22 @@
+import React from "react";
+import { flagChecker, getInput } from "./flagChecker";
+
+
 const App = () => {
     return(
         <div className ="App">
             <header>
                 <h1>please get the flag thanks</h1>
             </header>
+            <body>
+                <div>
+                    <p>i am bad at this ok</p>
+                    <form id="flag" onSubmit={flagChecker}>
+                        <input type="text" id="userInput"/>
+                        <input type="submit" id={getInput}/>
+                    </form>
+                </div>
+            </body>
         </div>
     );
 }
